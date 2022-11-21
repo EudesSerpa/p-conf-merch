ba97f0b2ece6cc8cbce006285fa2c3cecd44b5d2
Eudes Serpa <serpagame@gmail.com>
2022-11-21 18:24:30 -0500

    [fix] webpack prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,72 @@
 const path = require('path');
-const dotenv = require('dotenv');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = () => {
-  const env = dotenv.config().parsed;
-
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-
-  return {
-    mode: 'production',
-    entry: {
-      main: './src/index.js',
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@assets': path.resolve(__dirname, 'src/assets/'),
     },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].bundle.js',
-      publicPath: '/',
-      clean: true,
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-      alias: {
-        '@components': path.resolve(__dirname, 'src/components'),
-        '@pages': path.resolve(__dirname, 'src/pages/'),
-        '@assets': path.resolve(__dirname, 'src/assets/'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-          },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        use: { loader: 'html-loader' },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name]-[hash][ext]',
         },
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name]-[hash][ext]',
         },
-        {
-          test: /\.html$/i,
-          use: { loader: 'html-loader' },
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'assets/images/[name]-[hash][ext]',
-          },
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'assets/fonts/[name]-[hash][ext]',
-          },
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-        filename: './index.html',
-      }),
-      new MiniCssExtractPlugin({
-        filename: './[name].css',
-      }),
-      new webpack.DefinePlugin(envKeys),
+      },
     ],
-    optimization: {
-      minimize: true,
-      minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-    },
-  };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: './[name].css',
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
 };
