04be7bdc99ee574edcc5c4c0961c84661fb6a378
Eudes Serpa <serpagame@gmail.com>
2022-11-21 18:34:09 -0500

    [fix] action config and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+require('dotenv').config();
 
 module.exports = {
   mode: 'production',
@@ -63,6 +65,12 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: './[name].css',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        PAYPAL_CLIENT_ID: JSON.stringify(process.env.PAYPAL_CLIENT_ID),
+        GOOGLE_KEY_API: JSON.stringify(process.env.GOOGLE_KEY_API),
+      },
     }),
   ],
   optimization: {
